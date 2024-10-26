8ecc5ef38944dfee8a7683f4710fb7d90f97c80b
Isnubi <louis.gambart77270@gmail.com>
2024-10-26 17:25:35 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,62 @@
+import os
+from flask import Flask, render_template
+import requests
+from ics import Calendar
+from datetime import datetime, timedelta
+import pytz
+
+
+app = Flask(__name__)
+
+
+def get_ics_data(url: str) -> str | None:
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+
+        return Calendar(r.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
+
+def get_current_class(calendar: Calendar) -> str | None:
+    now = datetime.now(pytz.utc)
+    for event in calendar.timeline.now():
+            event.begin = event.begin + timedelta(hours=2)
+            event.end = event.end + timedelta(hours=2)
+            return event
+    return None
+
+
+def get_next_class(calendar: Calendar) -> str | None:
+    now = datetime.now(pytz.utc)
+    for event in calendar.timeline.start_after(now):
+        event.begin = event.begin + timedelta(hours=2)
+        event.end = event.end + timedelta(hours=2)
+        return event
+    return None
+
+
+@app.route('/')
+def classes_info():
+    ade_url = os.getenv('ADE_URL')
+
+    calendar = get_ics_data(ade_url)
+    if not calendar:
+        return "<p>Error fetching data<p>"
+
+    current_event = get_current_class(calendar)
+    next_event = get_next_class(calendar)
+
+    if current_event:
+        current_info = current_event
+    else:
+        current_info = None
+
+    if next_event:
+        next_info = next_event
+    else:
+        next_info = None
+
+    return render_template('index.html', current=current_info, next=next_info)
