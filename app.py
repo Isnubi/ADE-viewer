d46cf495cc6edb953db27259adb817c7fdf29135
Isnubi <louis.gambart77270@gmail.com>
2024-11-14 20:27:00 +0100

    Create a class for Calendar and Event in order to handle correctly the TZ problems

--- app.py
+++ app.py
@@ -1,64 +1,99 @@
 import os
 from flask import Flask, render_template
 import requests
-from ics import Calendar
+from ics import Calendar as ics_Calendar
 from datetime import datetime, timedelta
 import pytz
 
 
 app = Flask(__name__)
 
-def get_ics_data(url: str) -> str | None:
-    try:
-        r = requests.get(url)
-        r.raise_for_status()
 
-        return Calendar(r.text)
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+class Event:
+    def __init__(self, name: str, begin: datetime, end: datetime, location: str, duration: str):
+        self.name = name
+        self.begin = begin
+        self.end = end
+        self.location = location
+        self.duration = duration
+
+    def __dict__(self):
+        return {
+            'name': self.name,
+            'begin': self.begin,
+            'end': self.end,
+            'location': self.location,
+            'duration': self.duration
+        }
+
+
+class Calendar:
+    def __init__(self, url: str, timezone: pytz.timezone, timedelta: int):
+        self.url = url
+        self.timezone = timezone
+        self.timedelta = timedelta
+        self.calendar = self.get_calendar()
+
+    def get_calendar(self) -> str | None:
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            return ics_Calendar(response.text)
+        except requests.exceptions.RequestException as e:
+            return None
+
+    def adjust_event(self, event: str) -> Event:
+        event_begin = event.begin
+        event_end = event.end
+
+        event_begin = event_begin.astimezone(self.timezone)
+        event_end = event_end.astimezone(self.timezone)
+
+        event_begin += timedelta(hours=self.timedelta)
+        event_end += timedelta(hours=self.timedelta)
+
+        return Event(event.name, event_begin, event_end, event.location, event.duration)
+
+    def get_current_event(self) -> str | None:
+        now = datetime.now(self.timezone)
+        for event in self.calendar.timeline:
+            adjusted_event = self.adjust_event(event)
+            if adjusted_event.begin <= now <= adjusted_event.end:
+                return adjusted_event
         return None
 
+    def get_next_event(self) -> str | None:
+        now = datetime.now(self.timezone)
+        for event in self.calendar.timeline:
+            adjusted_event = self.adjust_event(event)
+            if now < adjusted_event.begin:
+                return adjusted_event
+        return None
 
-def get_current_class(calendar: Calendar, delta: int) -> str | None:
-    now = datetime.now(pytz.utc)
-    for event in calendar.timeline.now():
-        event.begin = event.begin + timedelta(hours=delta)
-        event.end = event.end + timedelta(hours=delta)
-        return event
-    return None
-
-
-def get_next_class(calendar: Calendar, delta: int) -> str | None:
-    now = datetime.now(pytz.utc)
-    for event in calendar.timeline.start_after(now):
-        print(event.begin)
-        print(event.end)
-        event.begin += timedelta(hours=delta)
-        event.end += timedelta(hours=delta)
-        return event
-    return None
 
 
 @app.route('/')
 def classes_info():
     ade_url = os.getenv('ADE_URL')
-    delta = 1
+    timezone = pytz.timezone(os.getenv('TZ', 'Europe/Paris'))
+    timedelta = int(os.getenv('TIME_DELTA', 0))
 
-    calendar = get_ics_data(ade_url)
+    calendar = Calendar(ade_url, timezone, timedelta)
     if not calendar:
         return "<p>Error fetching data<p>"
 
-    current_event = get_current_class(calendar, delta)
-    next_event = get_next_class(calendar, delta)
+    current_event = calendar.get_current_event()
+    next_event = calendar.get_next_event()
 
     if current_event:
-        current_info = current_event
+        current_info = current_event.__dict__()
     else:
         current_info = None
 
     if next_event:
-        next_info = next_event
+        next_info = next_event.__dict__()
     else:
         next_info = None
 
     return render_template('index.html', current=current_info, next=next_info)
+
